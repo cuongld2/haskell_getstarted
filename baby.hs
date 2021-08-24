e6dadc08d05c75dfddc9c6e7def0723399409de6
Le Dinh Cuong <v.cuongld7@vinbigdata.org>
2021-08-24 22:10:28 +0700

    Add baby file for containing demonstration of functions

---
+++ baby.hs
@@ -0,0 +1,35 @@
+doubleMe x=x+x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x=if x>100
+                    then x
+                    else x*2
+
+removeNonUppercase :: String -> String
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+factorial_recursive :: Integer -> Integer
+factorial_recursive 0 = 1
+factorial_recursive n = n*factorial_recursive (n-1)
+
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | bmi <= skinny = "You're underweight, you emo, you!"
+  | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | bmi <= fat = "You're fat! Lose some weight, fatty!"
+  | otherwise = "You're a whale, congratulations!"
+  where bmi = weight / height ^ 2
+        skinny = 18.5
+        normal = 25.0
+        fat = 30.0
+
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
